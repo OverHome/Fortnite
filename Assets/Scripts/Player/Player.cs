2e998d22d9aaf3dd5faa9a1b0b945ff010469794
OverHome <OverHome@users.noreply.github.com>
2025-02-11 01:43:10 +0300

    base vr online

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,10 @@
+using System;
 using FishNet.Object;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 namespace Player
 {
@@ -7,11 +12,33 @@ namespace Player
     {
         [field: SerializeField] public PlayerHealth Health { get; private set; }
         [field: SerializeField] public PlayerShooter Shooter { get; private set; }
-
+        
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            if (base.IsOwner)
+            {
+                GetComponent<XROrigin>().enabled = true;
+                GetComponent<InputActionManager>().enabled = true;
+                GetComponent<LocomotionSystem>().enabled = true;
+                GetComponent<ActionBasedContinuousMoveProvider>().enabled = true;
+                GetComponent<ActionBasedSnapTurnProvider>().enabled = true;
+                GetComponent<XROrigin>().CameraFloorOffsetObject.SetActive(true);
+            }
+            else
+            {
+                this.enabled = false;
+            }
+        }
         private void Awake()
         {
             Health.Initialize(this);
             Shooter.Initialize(this);
+        }
+
+        private void Update()
+        {
+            transform.GetChild(1).transform.rotation = GetComponent<XROrigin>().Camera.transform.rotation;
         }
 
         [ServerRpc]
